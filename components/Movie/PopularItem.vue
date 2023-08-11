c721f1a8a1014edfd3f639929d34e249205f3439
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-08-11 08:22:30 +0700

    movie page: sample item

---
+++ components/Movie/PopularItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+    const props = defineProps(['id', 'year', 'title', 'types', 'thumbnail'])
+    const id = props.id
+    const year = props.year
+    const title = props.title
+    const types = props.types
+    const thumbnail = props.thumbnail
+</script>
+
+<template>
+    <div class="post-202135 movie type-movie status-publish has-post-thumbnail hentry movie_genre-230 movie_genre-238 movie_genre-wmovie">
+        <div class="movie__poster">
+            <NuxtLink :to="{path: 'movie/' + id}" class="masvideos-LoopMovie-link masvideos-loop-movie__link movie__link">
+                <img width="300" height="450" :src="thumbnail.src" class="movie__poster--image" alt="" loading="lazy" :srcset="thumbnail.srcset">
+            </NuxtLink>
+        </div>
+        <div class="movie__body"><span class="movie__meta--release-year">{{ year }}</span>
+            <NuxtLink :to="{path: 'movie/' + id}" class="masvideos-LoopMovie-link masvideos-loop-movie__link movie__link">
+                <h3 class="masvideos-loop-movie__title movie__title">{{ title }}</h3>
+            </NuxtLink>
+            <span class="movie__meta--genre">
+                <template v-for="(name, index) in types" :key="index">
+                    <span v-if="index > 0">, </span>
+                    <NuxtLink :to="{path: 'movie-genre/' + name}" rel="tag">{{ name }}</NuxtLink>
+                </template>
+            </span>
+        </div>
+    </div>
+</template>
---
+++ components/Movie/Type.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+    const types = [
+        "action","animation","comedy","crime","documentary","drama","fantasy","horror","mystrey","romance","science fiction","sf","thriller","tv 영화","가족","공포","다큐멘터리","동양영화","드라마","로맨스","모험","미스터리","범죄","서부","서양영화","스릴러","애니메이션","액션","역사","음악","전쟁","코미디","판타지","한국영화"
+    ]
+
+    const route = useRoute();
+    let chosen = []
+    let chosenClass = ""
+    if (route.query.filter_genre && typeof route.query.filter_genre == 'string') {
+        route.query.filter_genre.split(",").forEach(name => chosen.push(name))
+        chosenClass += " " + chosen.join(" ")
+    }
+</script>
+
+<template>
+    <ul class="masvideos-widget-movies-layered-nav-list">
+        <li v-for="(name, index) in types" :key="index" :class="'masvideos-widget-movies-layered-nav-list__item masvideos-layered-nav-term ' + (chosen.includes(name) ? 'masvideos-widget-movies-layered-nav-list__item--chosen chosen' : '')">
+            <NuxtLink rel="nofollow" :to="{path: 'movie', query: { filter_genre: chosen.concat(name).join(','),  query_type_genre: 'or'}}">{{ name }}</NuxtLink>
+        </li>
+    </ul>
+</template>
