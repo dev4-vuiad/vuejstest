e63c2b87f73ab2cc8385384283b019af6159d54e
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-08-21 11:11:39 +0700

    change port 80

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  devServer: {
+    port: 80
+  },
   css: [
     '~/assets/css/style1.css',
     '~/assets/css/style2.css',
