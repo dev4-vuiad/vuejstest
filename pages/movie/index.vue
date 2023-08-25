d4a666e9d704e884a491533ff2c8f0da94d133ac
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-08-25 11:20:56 +0700

    fixed

--- pages/movie/index.vue
+++ pages/movie/index.vue
@@ -60,19 +60,22 @@
 </script>
 
 <template>
-    <body
-        class="archive post-type-archive post-type-archive-movie wp-custom-logo wp-embed-responsive masvideos masvideos-page masvideos-archive masvideos-js  sidebar-left dark">
+    <body class="archive post-type-archive post-type-archive-movie wp-custom-logo wp-embed-responsive masvideos masvideos-page masvideos-archive masvideos-js sidebar-left dark">
         <div id="page" class="hfeed site">
             <Header />
             <HeaderHandHeld />
             <div id="content" class="site-content " tabindex="-1">
                 <div class="container">
-                    <nav class="masvideos-breadcrumb"><a href="https://kokoatv.net">Home</a><span class="delimiter"><svg
+                    <nav class="masvideos-breadcrumb">
+                        <NuxtLink href="/">Home</NuxtLink>
+                        <span class="delimiter"><svg
                                 width="4px" height="7px">
                                 <path fill-rule="evenodd"
                                     d="M3.978,3.702 C3.986,3.785 3.966,3.868 3.903,3.934 L1.038,6.901 C0.920,7.022 0.724,7.029 0.598,6.916 L0.143,6.506 C0.017,6.393 0.010,6.203 0.127,6.082 L2.190,3.945 C2.276,3.829 2.355,3.690 2.355,3.548 C2.355,3.214 1.947,2.884 1.947,2.884 L1.963,2.877 L0.080,0.905 C-0.037,0.783 -0.029,0.593 0.095,0.479 L0.547,0.068 C0.671,-0.045 0.866,-0.039 0.983,0.083 L3.823,3.056 C3.866,3.102 3.875,3.161 3.885,3.218 C3.945,3.267 3.988,3.333 3.988,3.415 L3.988,3.681 C3.988,3.689 3.979,3.694 3.978,3.702 Z">
                                 </path>
-                            </svg></span>영화</nav>
+                            </svg>
+                        </span>영화
+                    </nav>
                     <div class="site-content__inner">
                         <div id="primary" class="content-area"> <!-- ads movies top -->
                             <div class="ads-achive-movies-top" style="text-align: center;">
@@ -88,7 +91,6 @@
                                         </div>
                                     </div>
                                 </div>
-                                <center style="margin-top:10px;margin-bottom:10px;" class="ads_cate_top"></center>
                             </div>
                             <header class="page-header">
                                 <h1 class="page-title">영화</h1>
