fda2eb2c595ce53218aa9cc0e01822eaa6572f53
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-08-31 10:54:21 +0700

    fixed

--- pages/tv-show-genre/[name]/index.vue
+++ pages/tv-show-genre/[name]/index.vue
@@ -61,7 +61,7 @@
         <HeaderHandHeld />
         <div id="content" class="site-content " tabindex="-1">
             <div class="container">
-                <TvshowsBreadScrumb base="tv-show-genre" :title="genre" :page="page * 1 > 1 ? page : undefined" />
+                <TvshowsBreadScrumb base="tv-show-genre" :title="getGenreTitle(genre, data.data.items[0].genres)" :page="page * 1 > 1 ? page : undefined" />
                 <div class="site-content__inner">
                     <div id="primary" class="content-area"> <!-- ads tv-show top -->
                         <div class="ads-achive-tvshow-top" style="text-align: center;">
