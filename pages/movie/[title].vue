a4804a603829485810dcea923083f052edbe05ca
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-09-07 11:12:23 +0700

    change api

--- pages/movie/[title].vue
+++ pages/movie/[title].vue
@@ -1,6 +1,6 @@
 <script setup>
     const route = useRoute()
-    const title = ref(route.params.title)
+    const title = route.params.title
 
     definePageMeta({
         layout: 'movie',
@@ -49,23 +49,12 @@
     })
 
     const { data }  = await useAsyncData(
-        () => $fetch('https://backendnew.takitv.net/api/movies', {
-            params: {
-                title: title.value
-            }
-        }),
-        {
-            watch: [title]
-        }
+        () => $fetch('https://backendnew.takitv.net/api/movies/' + encodeURIComponent(title))
     )
 
     useHead({
-        title: title.value + ' – 코코아티비 :: KOKOA.TV'
+        title: title + ' – 코코아티비 :: KOKOA.TV'
     });
-
-    const lastGenre = (genres) => {
-        return genres[genres.length - 1]
-    }
 
 </script>
 <template>
@@ -77,19 +66,19 @@
                         class="post-202135 movie type-movie status-publish has-post-thumbnail hentry movie_genre-230 movie_genre-238 movie_genre-wmovie">
                         <div class="single-movie__player-container stretch-full-width">
                             <div class="single-movie__player-container--inner container">
-                                <MovieBreadScrumb v-if="data && data.data && data.data.items.length && data.data.items[0].genres.length" :genre="lastGenre(data.data.items[0].genres)" :title="data.data.items[0].title" />
+                                <MovieBreadScrumb v-if="data && data.genres && data.genres.length" :genre="data.genres[data.genres.length - 1]" :title="data.title" />
                                 <div class="ads-movie-top" style="text-align: center;margin-bottom: 10px;">
                                 </div>
                                 <div class="single-movie__row row">
-                                    <MovieIntroInfo v-if="data && data.data && data.data.items && data.data.items[0]"
-                                        :year="data.data.items[0].year"
-                                        :duration="data.data.items[0].duration"
-                                        :title="data.data.items[0].title"
-                                        :originalTitle="data.data.items[0].originalTitle"
-                                        :genres="data.data.items[0].genres"
-                                        :src="data.data.items[0].src"
-                                        :description="data.data.items[0].description"
-                                        :outlink="data.data.items[0].outlink"
+                                    <MovieIntroInfo v-if="data && data.id"
+                                        :year="data.year"
+                                        :duration="data.duration"
+                                        :title="data.title"
+                                        :originalTitle="data.originalTitle"
+                                        :genres="data.genres"
+                                        :src="data.src"
+                                        :description="data.description"
+                                        :outlink="data.outlink"
                                     />
                                     <div class="single-movie-ads-box">
                                         <div class="ads-box-child">
@@ -101,7 +90,7 @@
                         <section class="movie__related">
                             <div class="movie__related--inner">
                                 <h2 class="movie__related--title">관련 컨텐츠</h2>
-                                <MovieIntroRelatedList v-if="data && data.data && data.data.items && data.data.items[0]" :data="data.data.items[0].relateds" />
+                                <MovieIntroRelatedList v-if="data && data.relateds" :data="data.relateds" />
                             </div>
                         </section>
                         <div style="text-align: center;margin-top: 10px;"></div>
