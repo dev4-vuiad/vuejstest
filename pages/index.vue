919daac86e8d32b57b3f6bc4353a3f276178d37a
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-09-25 11:25:18 +0700

    fix ads on home page

--- pages/index.vue
+++ pages/index.vue
@@ -186,13 +186,14 @@
 
     onMounted(() => {
         //Put ads
-        if ($isProdEnv()) { 
+        if ($isProdEnv() && !window.adsLoaded) { 
             (function(s, w) {
                 s.setAttribute("async", "async");
                 s.setAttribute("type", "text/javascript");
                 s.setAttribute("src", "//scripts.kiosked.com/loader/kiosked-loader.js?site=17622");
                 w.document.body.appendChild(s);
             })(window.top.document.createElement("script"), window.top)
+            window.adsLoaded = true
         }
     })
 </script>
