3d48edf5d70e18a448ae6dbd87c97101d8d35fbc
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-09-25 11:18:19 +0700

    ad flag

--- pages/movie/index.vue
+++ pages/movie/index.vue
@@ -146,6 +146,7 @@
                 s.setAttribute("src", "//scripts.kiosked.com/loader/kiosked-loader.js?site=17622");
                 w.document.body.appendChild(s);
             })(window.top.document.createElement("script"), window.top)
+            window.adsLoaded = true
         }
     })
     
