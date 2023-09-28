2b0ed3a724e478a1e98d6e05283e666737119dd5
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-09-28 13:56:57 +0700

    fix

--- components/Header/HeaderHandHeld.vue
+++ components/Header/HeaderHandHeld.vue
@@ -30,7 +30,7 @@
                                                     <ul id="menu-main-menu-2" class="offcanvas-nav yamm">
                                                         <li itemscope="itemscope"
                                                             itemtype="https://www.schema.org/SiteNavigationElement"
-                                                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8092 nav-item show">
+                                                            class="menu-item menu-item-type-post_type menu-item-object-page nav-item show">
                                                             <NuxtLink title="영화" to="/movie/" class="nav-link show">
                                                                 <span class="dashicons fas fa-film after-menu-image-icons"></span>
                                                                 <span class="menu-image-title-after menu-image-title">영화</span>
@@ -38,7 +38,7 @@
                                                         </li>
                                                         <li itemscope="itemscope"
                                                             itemtype="https://www.schema.org/SiteNavigationElement"
-                                                            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children dropdown menu-item-161947 nav-item show">
+                                                            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children dropdown nav-item show">
                                                             <NuxtLink title="TV" to="/tv-shows/" data-toggle="dropdown"
                                                                 aria-haspopup="true" aria-expanded="false"
                                                                 class="dropdown-toggle nav-link">
@@ -48,19 +48,19 @@
                                                             <ul class="dropdown-menu" role="menu">
                                                                 <li itemscope="itemscope"
                                                                     itemtype="https://www.schema.org/SiteNavigationElement"
-                                                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-8093 nav-item show">
+                                                                    class="menu-item menu-item-type-taxonomy menu-item-object-category nav-item show">
                                                                     <NuxtLink title="드라마" to="/k-drama/"
                                                                         class="dropdown-item show">드라마</NuxtLink>
                                                                 </li>
                                                                 <li itemscope="itemscope"
                                                                     itemtype="https://www.schema.org/SiteNavigationElement"
-                                                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-8094 nav-item show">
+                                                                    class="menu-item menu-item-type-taxonomy menu-item-object-category nav-item show">
                                                                     <NuxtLink title="예능" to="/k-show/"
                                                                         class="dropdown-item show">예능</NuxtLink>
                                                                 </li>
                                                                 <li itemscope="itemscope"
                                                                     itemtype="https://www.schema.org/SiteNavigationElement"
-                                                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-8095 nav-item show">
+                                                                    class="menu-item menu-item-type-taxonomy menu-item-object-category nav-item show">
                                                                     <NuxtLink title="시사다큐" to="/k-sisa/"
                                                                         class="dropdown-item show">시사</NuxtLink>
                                                                 </li>
@@ -68,17 +68,46 @@
                                                         </li>
                                                         <li itemscope="itemscope"
                                                             itemtype="https://www.schema.org/SiteNavigationElement"
-                                                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-118282 nav-item show">
+                                                            class="menu-item menu-item-type-taxonomy menu-item-object-category nav-item show">
                                                             <NuxtLink title="미드" to="/u-drama/" class="nav-link show">미드</NuxtLink>
                                                         </li>
                                                         <li itemscope="itemscope"
                                                             itemtype="https://www.schema.org/SiteNavigationElement"
-                                                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-8098 nav-item show">
+                                                            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children dropdown nav-item show">
                                                             <NuxtLink title="OTT/Web" to="/ott-web/"
                                                                 class="nav-link show">
                                                                 <span class="dashicons fas fa-cube after-menu-image-icons"></span>
                                                                 <span class="menu-image-title-after menu-image-title">OTT</span>
                                                             </NuxtLink>
+                                                            <ul class="dropdown-menu" role="menu">
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="Amazon Prime Video" to="/ott-web/amazon-prime-video/" key="k-drama">Amazon Prime Video</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="Apple TV" to="/ott-web/apple-tv/" key="apple-tv">Apple TV</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="Disney +" to="/ott-web/disney/" key="disney">Disney +</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="HBO max" to="/ott-web/hbo-max/" key="hbo-max">HBO max</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="Netflix" to="/ott-web/netflix/" key="netflix">Netflix</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="Peacock" to="/ott-web/peacock/" key="peacock">Peacock</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="Tving" to="/ott-web/tving/" key="tving">Tving</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="wavve" to="/ott-web/wavve/" key="wavve">wavve</NuxtLink>
+                                                                </li>
+                                                                <li class="menu-item" itemscope="itemscope">
+                                                                    <NuxtLink title="watcha" to="/ott-web/watcha/" key="watcha">watcha</NuxtLink>
+                                                                </li>
+                                                            </ul>
                                                         </li>
                                                     </ul>
                                                 </div>
