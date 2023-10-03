45e0c53d1f7a44b18d85862c25bee3e2edd0ecc4
Lâm Thành Danh <germayu@gmail.com>
2023-10-03 22:48:59 +0700

    remove class wp-embed-responsive

--- layouts/tvshow.vue
+++ layouts/tvshow.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="page" class="tv_show-template-default single single-tv_show postid-203734 wp-custom-logo wp-embed-responsive masvideos masvideos-page masvideos-single masvideos-js single-tv_show-v1 full-width dark">
+    <div id="page" class="tv_show-template-default single single-tv_show wp-custom-logo masvideos masvideos-page masvideos-single masvideos-js single-tv_show-v1 full-width dark">
         <Header />
         <HeaderHandHeld />
         <slot />
