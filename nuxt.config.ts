6febb5e8e2f39acdffc6460a6e1a1ba04b01f44f
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-10-10 15:39:17 +0700

    complete search feature and fix sidebar on person not working on mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ export default defineNuxtConfig({
         },
         {
           rel: 'stylesheet',
-          href: '/css/style.css?fagag'
+          href: '/css/style.css?hrhyee'
         },
       ],
       script: [
--- pages/person/index.vue
+++ pages/person/index.vue
@@ -8,31 +8,7 @@
         pageTransition: {
             name: 'page', 
             mode: 'out-in',
-            onAfterEnter: () => {
-                //animated drop down submenu
-                $(".site_header__primary-nav .menu-item, .site_header__secondary-nav .menu-item, .site_header__secondary-nav-v3 .menu-item, .site_header__navbar-primary .menu-item").on("mouseenter", function() {
-                    var e = $(this)
-                    , t = e.parents(".site_header__primary-nav, .site_header__secondary-nav, .site_header__secondary-nav-v3, .site_header__navbar-primary")
-                    , a = e.parents(".vodi-animate-dropdown");
-                    if (0 < t.length && (a = t),
-                    e.hasClass("menu-item-has-children"))
-                        a.hasClass("animated-dropdown") || setTimeout(function() {
-                            a.addClass("animated-dropdown")
-                        }, 200);
-                    else if (a.hasClass("animated-dropdown")) {
-                        e.parents(".menu-item-has-children").length <= 0 && a.removeClass("animated-dropdown")
-                    }
-                })
-
-                //Sidebar menu
-                $(".site-header__offcanvas .navbar-toggler").on("click", function() {
-                    0 < $(this).parents(".stuck").length && $("html, body").animate({
-                        scrollTop: $("body")
-                    }, 0),
-                    $(this).closest(".site-header__offcanvas").toggleClass("toggled"),
-                    $("body").toggleClass("off-canvas-active")
-                })
-                
+            onAfterEnter: () => {                
                 $(document).on("click", function(e) {
                     $(".site-header__offcanvas").hasClass("toggled") && ($(".navbar-toggler").is(e.target) || 0 !== $(".navbar-toggler").has(e.target).length || $(".offcanvas-collapse").is(e.target) || 0 !== $(".offcanvas-collapse").has(e.target).length || ($(".site-header__offcanvas").removeClass("toggled"),
                     $("body").removeClass("off-canvas-active")))
@@ -55,6 +31,7 @@
 
     const orderBy = ref(route.query.orderBy || '')
     const page = ref(route.query.page || 1)
+    const search = ref(route.query.search || '')
 
     const defaultData = {
         total: 0,
@@ -66,33 +43,47 @@
     const { data, pending }  = useLazyAsyncData(
         () => $fetch($apiBaseUrl() + '/casts', {
             params: {
-                orderBy: orderBy.value || undefined,
+                search: search.value,
+                orderBy: orderBy.value,
                 page: page.value
             }
         }),
         {
             default: () => defaultData,
-            watch: [orderBy, page]
+            watch: [orderBy, page, search]
         }
     )
 
     const onChangeOrderBy = (event) => {
         let val = event.target.value
         let query = Object.assign({}, route.query)
-        query.orderBy = val
-        query.page = 1
+        query.orderBy = val.length ? val : undefined
+        query.page = query.page > 1 ? query.page : undefined
         router.push({query: query})
-        orderBy.value = val
+        orderBy.value = val.length ? val : undefined
         page.value = 1
         nextTick()
     }
 
     const onSelectPage = function(val) {
         let query = Object.assign({}, route.query)
-        query.page = val
+        query.page = val && val > 1 ? val : undefined
         router.push({query: query})
         page.value = val
         $('#scrollUp').trigger('click')
+    }
+
+    const onSearchChange = (event) => {
+        let val = event.target.value
+        let query = Object.assign({}, route.query)
+        query.page = undefined
+        query.orderBy = undefined
+        query.search = val.length ? val : undefined
+        router.push({query: query})
+        orderBy.value = ''
+        page.value = 1
+        search.value = val
+        nextTick()
     }
 </script>
 
@@ -115,6 +106,12 @@
                         <main id="main" class="site-main" role="main">
                             <header class="page-header">
                                 <h1 class="page-title">Person</h1>
+                                <div class="search">
+                                    <form role="search" method="get" class="search-form masvideos-search masvideos-search-tv_show" onsubmit="return false">
+                                        <input type="search" value="" class="search-field ui-autocomplete-input" placeholder="배우 검색 ..." autocomplete="off" @change="onSearchChange">
+                                        <button type="submit" class="search-submit"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path d="M7 0C11-0.1 13.4 2.1 14.6 4.9 15.5 7.1 14.9 9.8 13.9 11.4 13.7 11.7 13.6 12 13.3 12.2 13.4 12.5 14.2 13.1 14.4 13.4 15.4 14.3 16.3 15.2 17.2 16.1 17.5 16.4 18.2 16.9 18 17.5 17.9 17.6 17.9 17.7 17.8 17.8 17.2 18.3 16.7 17.8 16.4 17.4 15.4 16.4 14.3 15.4 13.3 14.3 13 14.1 12.8 13.8 12.5 13.6 12.4 13.5 12.3 13.3 12.2 13.3 12 13.4 11.5 13.8 11.3 14 10.7 14.4 9.9 14.6 9.2 14.8 8.9 14.9 8.6 14.9 8.3 14.9 8 15 7.4 15.1 7.1 15 6.3 14.8 5.6 14.8 4.9 14.5 2.7 13.6 1.1 12.1 0.4 9.7 0 8.7-0.2 7.1 0.2 6 0.3 5.3 0.5 4.6 0.9 4 1.8 2.4 3 1.3 4.7 0.5 5.2 0.3 5.7 0.2 6.3 0.1 6.5 0 6.8 0.1 7 0ZM7.3 1.5C7.1 1.6 6.8 1.5 6.7 1.5 6.2 1.6 5.8 1.7 5.4 1.9 3.7 2.5 2.6 3.7 1.9 5.4 1.7 5.8 1.7 6.2 1.6 6.6 1.4 7.4 1.6 8.5 1.8 9.1 2.4 11.1 3.5 12.3 5.3 13 5.9 13.3 6.6 13.5 7.5 13.5 7.7 13.5 7.9 13.5 8.1 13.5 8.6 13.4 9.1 13.3 9.6 13.1 11.2 12.5 12.4 11.4 13.1 9.8 13.6 8.5 13.6 6.6 13.1 5.3 12.2 3.1 10.4 1.5 7.3 1.5Z"></path></svg><span class="screen-reader-text">Search</span></button>
+                                    </form>
+                                </div>
                             </header>
                             <div class="vodi-control-bar">
                                 <div class="vodi-control-bar__right">
