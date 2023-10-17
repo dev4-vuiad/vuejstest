0df30c700604208cda75f558a1ed5b0dc07d1f55
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-10-17 11:31:27 +0700

    change back to prev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -104,7 +104,11 @@ export default defineNuxtConfig({
       head: {
         script: [
           {
-            children: '(function(s, w) {s.setAttribute("async", "async");s.setAttribute("type", "text/javascript");s.setAttribute("src", "//scripts.kiosked.com/loader/kiosked-loader.js?site=17622");w.document.body.appendChild(s);})(window.top.document.createElement("script"), window.top)'
+            children: 'window.__ITGS_started = Date.now();'
+          },
+          {
+            src: '//scripts.kiosked.com/loader/kiosked-loader.js?site=17622',
+            async: true
           }
         ]
       }
