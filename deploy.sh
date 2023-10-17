87a2f18715eba83e1f298b472f6fbae58f65378f
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-10-17 13:25:18 +0700

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #run git pull
-git pull
+git pull https://dev4.vuiad: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@github.com/dev4-vuiad/vuejstest.git
 
 #run git pull
 npm install
@@ -12,3 +12,9 @@ pm2 delete takitv
 
 #run start pm2
 pm2 start ecosystem.config.js
+
+#kil current process
+fuser -k 3000/tcp
+
+#clear frontend cache
+curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://localhost:3000/__nuxt_multi_cache/purge/all
