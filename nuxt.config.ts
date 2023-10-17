350f0c24beb273a72b3770119a4eaa612ec57114
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-10-17 11:10:18 +0700

    change head script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -104,11 +104,7 @@ export default defineNuxtConfig({
       head: {
         script: [
           {
-            children: 'window.__ITGS_started = Date.now();'
-          },
-          {
-            src: '//scripts.kiosked.com/loader/kiosked-loader.js?site=17622',
-            async: true
+            children: '(function(s, w) {s.setAttribute("async", "async");s.setAttribute("type", "text/javascript");s.setAttribute("src", "//scripts.kiosked.com/loader/kiosked-loader.js?site=17622");w.document.body.appendChild(s);})(window.top.document.createElement("script"), window.top)'
           }
         ]
       }
