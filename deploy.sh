688d7250665d183266dbbab339cf3f13a01cc5c1
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-10-20 08:28:58 +0700

    fix deployment file

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #run git pull
 git pull
 
+#run git pull
+npm install
+
 #run npm build
 npm run build
 
 #run delete pm2
-pm2 delete test_ads
+pm2 delete takitv
 
 #run start pm2
 pm2 start ecosystem.config.js
 
 #kil current process
-fuser -k 3006/tcp
+fuser -k 3000/tcp
 
 #clear frontend cache
-curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://localhost:3006/__nuxt_multi_cache/purge/all
+curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://localhost:3000/__nuxt_multi_cache/purge/all
