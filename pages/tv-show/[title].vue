72c2d10e0b44b6e532252cd2ad117b0d0dba5753
vvc19121990 <vvc19121990@yahoo.com>
2023-10-30 10:50:19 +0700

    add description for SEO

--- pages/tv-show/[title].vue
+++ pages/tv-show/[title].vue
@@ -57,7 +57,14 @@
     )
 
     useHead({
-        title: title + ' – 코코아티비 :: KOKOA.TV',
+        title: title + ' 코코아티비 :: KOKOATV.NET',
+        meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: title + ' - 최신 한국드라마,미드, 예능,시사 스트리밍 다시보기 사이트 : 코코아티비 :: KOKOATV.NET 에서 무료로 즐기세요, 넷플릭스, 와차, 디즈니 플러스등 각종 OTT 컨텐츠를 감상하실 수 있습니다.'
+            }
+        ],
         script: [
             {
                 children: 'function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-NL156SRJ6P"),gtag("config","UA-160268616-2");'
