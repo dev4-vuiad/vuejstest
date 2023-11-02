a5e620fa2a2d6aba1f99fdd86baee98aa4afc976
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-11-02 16:19:42 +0700

    fix slider bottom of tv show intro page

--- components/Tvshow/RelatedSection.vue
+++ components/Tvshow/RelatedSection.vue
@@ -16,6 +16,23 @@
         
     )
 
+    onMounted(() => {
+        let slider = $('#tv-show-related').find('.tv-shows__inner').slick({
+            "slidesToShow":5,
+            "slidesToScroll":5,
+            "dots":false,
+            "arrows":true,
+            "autoplay":false,
+            "responsive":[{"breakpoint":767,"settings":{"slidesToShow":2,"slidesToScroll":2}},{"breakpoint":992,"settings":{"slidesToShow":3,"slidesToScroll":3}},{"breakpoint":1200,"settings":{"slidesToShow":4,"slidesToScroll":4}}]
+        })
+        $('#tv-show-related').find('button.slick-next').on('click', function() {
+            slider.slick('slickNext')
+        })
+        $('#tv-show-related').find('button.slick-prev').on('click', function() {
+            slider.slick('slickPrev')
+        })
+    })
+
 </script>
 
 <template>
