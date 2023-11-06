a97845139d5cba618976d33fd735205f5ee9b91a
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-11-06 15:01:55 +0700

    update outlink

--- pages/episode/[title].vue
+++ pages/episode/[title].vue
@@ -172,8 +172,8 @@
                                             :casts="data.casts"
                                         />
                                         <div style="margin-bottom:15px;">
-                                            <button v-if="outlink && outlink.includes('t01.bmytv.com')" class="btn-outlink" @click="onWatching">바로보기</button>
-                                            <a v-else :href="outlink" class="a_btn_out">
+                                            <button v-if="data.outlink && data.outlink.includes('t01.bmytv.com')" class="btn-outlink" @click="onWatching">바로보기</button>
+                                            <a v-else :href="data.outlink" class="a_btn_out">
                                                 <button class="btn-outlink">바로보기</button>
                                             </a>
                                         </div>
