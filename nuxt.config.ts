6f053be2d5e6127598fb963e0e670733bd99cc50
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-11-06 15:13:05 +0700

    fix watch page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,7 +68,7 @@ export default defineNuxtConfig({
         },
         {
           rel: 'stylesheet',
-          href: '/css/watch.css?gsdgdwsgfs'
+          href: '/css/watch.css?gdshdshdfgewr'
         }
       ],
       script: [
