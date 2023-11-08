f3e6a36219a65e9f1a200bf562362b42ccaf65d6
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-11-08 16:29:39 +0700

    fix google analytics: change codes to body

--- pages/person/index.vue
+++ pages/person/index.vue
@@ -29,7 +29,8 @@
         title: 'Person – 코코아티비 :: KOKOA.TV',
         script: [
             {
-                children: 'function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-NL156SRJ6P"),gtag("config","UA-160268616-2");'
+                children: 'function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-NL156SRJ6P"),gtag("config","UA-160268616-2");',
+                body: true
             }
         ]
     });
--- pages/tv-show-genre/[name]/index.vue
+++ pages/tv-show-genre/[name]/index.vue
@@ -90,7 +90,8 @@
         ],
         script: [
             {
-                children: 'function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-NL156SRJ6P"),gtag("config","UA-160268616-2");'
+                children: 'function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-NL156SRJ6P"),gtag("config","UA-160268616-2");',
+                body: true
             }
         ]
     });
