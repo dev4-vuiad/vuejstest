9be628a5130d2a59c42355c11afc69a3bd763de6
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-11-13 14:59:48 +0700

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,6 @@ pm2 start ecosystem.config.js --watch
 
 #clear frontend cache
 curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://127.0.0.1:3003/__nuxt_multi_cache/purge/all
+
+#kill current process
+fuser -k 3003/tcp
