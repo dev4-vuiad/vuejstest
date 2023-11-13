8ea682e4466bb7abb52fcd10bd07caf5a12a3be7
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-11-13 13:07:13 +0700

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,8 @@ npm install
 #run npm build
 npm run build
 
-#run delete pm2
-pm2 delete takitv
-
 #run start pm2
-pm2 start ecosystem.config.js
-
-#kil current process
-fuser -k 3000/tcp
+pm2 start ecosystem.config.js --watch
 
 #clear frontend cache
 curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://127.0.0.1:3000/__nuxt_multi_cache/purge/all
