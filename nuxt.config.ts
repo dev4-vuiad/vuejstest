b5c5228726447a9c82bd30ee0974151311e943ab
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-11-22 10:33:21 +0700

    update cloudflaire cache fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
         { rel: 'apple-touch-icon', type: 'image/x-icon', href: 'https://image002.modooup.com/wp-content/uploads/2023/03/ms-icon-310x310-1-300x300.png' },
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800%7cOpen+Sans:400,600,700&#038;subset=latin%2Clatin-ext'
+          href: '/css/fonts.css?gsderwygg'
         },
         {
           rel: 'stylesheet',
