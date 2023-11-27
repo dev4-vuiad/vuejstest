ba7ab103f9a9611a35e4b472564f7ab1f24a7845
Lâm Thành Danh <germayu@gmail.com>
2023-11-27 23:26:18 +0700

    update css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,7 +66,7 @@ export default defineNuxtConfig({
         },
         {
           rel: 'stylesheet',
-          href: '/css/style.css?dshfdnnc'
+          href: '/css/style.css?fasgwtw'
         },
         {
           rel: 'stylesheet',
