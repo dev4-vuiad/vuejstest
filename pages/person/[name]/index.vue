f8747e216efb2882f3442ec57705abf20d6ce9d1
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-12-06 16:47:39 +0700

    update category single person page items

--- pages/person/[name]/index.vue
+++ pages/person/[name]/index.vue
@@ -138,7 +138,7 @@
                                             :srcSet="item.srcSet"
                                             :seasonNumber="item.seasonNumber"
                                             :episodeNumber="item.episodeNumber"
-                                            :link="item.link"
+                                            :category="item.category"
                                             :postType="item.postType"
                                         />
                                     </div>
