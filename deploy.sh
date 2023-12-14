17dfc8506524d93feb0f035c6907551d11ed66c6
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-12-14 12:46:23 +0700

    update ott movie category

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,7 @@ npm install
 npm run build
 
 #run start pm2
-pm2 start ecosystem.config.js --watch
+pm2 restart ecosystem.config.js
 
 #clear frontend cache
 curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://127.0.0.1:3004/__nuxt_multi_cache/purge/all
-
-#kill current process
-fuser -k 3003/tcp
