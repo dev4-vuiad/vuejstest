5cff1547600cf7a8ef9c1a527a09655814d7323f
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2024-03-11 10:13:32 +0700

    remove unused script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -89,9 +89,6 @@ export default defineNuxtConfig({
         },
         {
           src: '/js/jquery.easing.min.js'
-        },
-        {
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-SHE23J6734'
         }
       ]
     }
