f9144efd565e3938a8364454368cd4aa9adb7347
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2024-03-14 16:30:03 +0700

    fix search page poster not same height

--- components/Search/MovieItem.vue
+++ components/Search/MovieItem.vue
@@ -38,20 +38,15 @@
                 Featured
             </span>
         </div>
-        <div class="tv-show__poster box-phim">
+        <div class="tv-show__poster box-phim loading-bg">
                 <NuxtLink :to="'/episode/' + slug" class="masvideos-LoopTvShow-link masvideos-loop-tv-show__link tv-show__link">
-                    <img :src="src" class="tv-show__poster--image tv_show__poster--image" :key="renderCount">		                                                     
+                    <img :src="src" class="tv-show__poster--image tv_show__poster--image lazyload" :key="renderCount">		                                                     
                 <span style="display:none" class="span_sea_ep_title box-ep">{{ $getEpTxt(seasonNumber, episodeNumber) }}</span></NuxtLink>
                 <div class="box-tv_show">{{ category }}</div>
         </div>
         <div class="tv-show__body">
             <div class="tv-show__info">
                 <div class="tv-show__info--head">
-                    <div class="tv-show__meta">
-                        <div class="tv-show__meta--release-year">
-                        
-                        </div>
-                    </div>
                     <NuxtLink :to="'/episode/' + slug" class="masvideos-LoopTvShow-link masvideos-loop-tv-show__link tv-show__link">
                         <h3 class="masvideos-loop-tv-show__title  tv-show__title">
                             {{ title }}		                                                                
@@ -65,7 +60,7 @@
     <div v-else class="movie type-movie status-publish has-post-thumbnail hentry" :postid="id">
         <div class="movie__poster box-phim">
             <NuxtLink :to="'/movie/' + slug" class="masvideos-LoopMovie-link masvideos-loop-movie__link movie__link">
-                <img :src="src" :srcset="srcSet" class="movie__poster--image" :key="renderCount">
+                <img :src="src" :srcset="srcSet" class="movie__poster--image lazyload" :key="renderCount">
             </NuxtLink>
             <div class="box-movie">영화</div>
         </div>
