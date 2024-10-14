92221b710a577695f628231b5707c8dd5493ae74
tgao0606 <tgao060@gmail.com>
2024-10-14 17:29:48 +0700

    edit deploy sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ pm2 delete index
 pm2 start ecosystem.config.js --watch
 
 #clear frontend cache
-curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://127.0.0.1:3001/__nuxt_multi_cache/purge/all
+curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://localhost:3001/__nuxt_multi_cache/purge/all
 
 #kill current process
 fuser -k 3001/tcp
